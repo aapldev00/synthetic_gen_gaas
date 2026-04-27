e4e3db134b738901762784c41ba1be3f5aee309d
AAPL00 <aapldev00@proton.me>
2026-04-27 10:24:12 -0400

    feat(generator): add UUID v4 provider with auto-registration
    - Implement UUIDFactory following the GeneratorFactory interface.
    - Use google/uuid for high-performance unique ID generation.
    - Implement self-registration via init() function.

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/aapldev00/synthetic_gen_gaas
 go 1.26.2
 
 require (
+	github.com/google/uuid v1.6.0
 	google.golang.org/grpc v1.80.0
 	google.golang.org/protobuf v1.36.11
 )
---
+++ internal/generator/providers/uuid.go
@@ -0,0 +1,21 @@
+package providers
+
+import (
+	"github.com/aapldev00/synthetic_gen_gaas/internal/generator"
+	"github.com/aapldev00/synthetic_gen_gaas/pkg/genproto"
+	"github.com/google/uuid"
+)
+
+// init is a special Go function that runs automatically when the package is loaded.
+// We use it to register this provider in the central registry.
+func init() {
+	generator.Register("uuid", UUIDFactory)
+}
+
+// UUIDFactory creates a new GeneratorFunc for UUID v4.
+// Since UUIDs don't require configuration for now, it simply ignores options.
+func UUIDFactory(options []*genproto.Option) (generator.GeneratorFunc, error) {
+	return func() string {
+		return uuid.NewString()
+	}, nil
+}
