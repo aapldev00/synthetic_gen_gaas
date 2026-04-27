c3f8198bb530be4a342955708a4e198714170f0c
AAPL00 <aapldev00@proton.me>
2026-04-27 12:04:19 -0400

    feat(generator): integrate local entropy source into GeneratorFactory
    - Update GeneratorFactory signature to accept *rand.Rand (math/rand/v2).
    - Enable deterministic data generation by injecting local entropy sources.
    - Optimize concurrent performance by eliminating global random mutex contention.
    - Refactor uuid provider to satisfy the new interface signature. (repeat)

--- internal/generator/providers/uuid.go
+++ internal/generator/providers/uuid.go
@@ -1,20 +1,24 @@
 package providers
 
 import (
+	"math/rand/v2"
+
 	"github.com/aapldev00/synthetic_gen_gaas/internal/generator"
 	"github.com/aapldev00/synthetic_gen_gaas/pkg/genproto"
 	"github.com/google/uuid"
 )
 
-// init is a special Go function that runs automatically when the package is loaded.
-// We use it to register this provider in the central registry.
+// init registers the uuid provider factory into the central registry
+// during the package initialization phase.
 func init() {
 	generator.Register("uuid", UUIDFactory)
 }
 
-// UUIDFactory creates a new GeneratorFunc for UUID v4.
-// Since UUIDs don't require configuration for now, it simply ignores options.
-func UUIDFactory(options []*genproto.Option) (generator.GeneratorFunc, error) {
+// UUIDFactory returns a GeneratorFunc that produces random UUID v4 strings.
+// The provided rand.Rand source is ignored as the underlying implementation
+// utilizes its own cryptographic entropy, but the parameter is maintained
+// to satisfy the GeneratorFactory interface signature.
+func UUIDFactory(r *rand.Rand, options []*genproto.Option) (generator.GeneratorFunc, error) {
 	return func() any {
 		return uuid.NewString()
 	}, nil
